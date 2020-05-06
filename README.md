99cb9757bafed281502014552aba11135eabb84e
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2020-05-06 14:07:39 +0300

    Add build directory

---
+++ README.md
@@ -0,0 +1,2 @@
+APPLICATION DEPLOYED ON HEROKU 
+LINK: https://safe-wave-54214.herokuapp.com/api/persons
---
+++ build/precache-manifest.ebb4ab3ae3efc8732d778d9cfb9aca8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4928c57c9c586ed6ed700379a7d0bcba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eda05de8e652be3e53a4",
+    "url": "/static/css/main.997a07d8.chunk.css"
+  },
+  {
+    "revision": "26401cc9dfbaf027cfc1",
+    "url": "/static/js/2.da5343fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da5343fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda05de8e652be3e53a4",
+    "url": "/static/js/main.f5bfd761.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
